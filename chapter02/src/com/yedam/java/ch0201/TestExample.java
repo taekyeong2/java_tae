c30814c160d8e65a8e6a1896908cd2a19883dc68
Taekyeong Kim <116075599+taekyeong2@users.noreply.github.com>
2022-11-04 10:36:11 +0900

    Update TestExample.java

--- chapter02/src/com/yedam/java/ch0201/TestExample.java
+++ chapter02/src/com/yedam/java/ch0201/TestExample.java
@@ -2,8 +2,8 @@ package com.yedam.java.ch0201;
 
 public class TestExample {
 
-	public static void main(String[] args) {
-		System.out.println("Hello, Java !!");  // 자바 실행
+	public static void main(String[] args) {  // 자바 실행 / main -> ctrl+ 
+		System.out.println("Hello, Java !!"); 
 
 	}
 
